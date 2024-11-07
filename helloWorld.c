c7cb49a2dbaebd86b22716f9bac3be48a613aa0a
Carlo <carlodelos90@gmail.com>
2024-11-07 21:36:02 +0100

    Hello World in C

---
+++ helloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+// Explanation: C uses #include to import libraries; here, <stdio.h> provides printf(), which is used for formatted output. The \n at the end adds a newline. The main function returns 0 to indicate successful execution.
