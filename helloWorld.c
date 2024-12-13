5ed84fa1adc996bcb6700c02bf8509a9e45f4789
carlodelos90 <carlodelos90@gmail.com>
2024-12-13 22:46:04 +0100

    Creating C project template for future uses to clone

--- helloWorld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello, World!\n");
-    return 0;
-}
-
-
-// Explanation: C uses #include to import libraries; here, <stdio.h> provides printf(), which is used for formatted output. The \n at the end adds a newline. The main function returns 0 to indicate successful execution.
